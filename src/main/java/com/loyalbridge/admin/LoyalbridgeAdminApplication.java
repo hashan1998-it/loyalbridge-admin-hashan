9199e94c465834782324dd6ad1a096c9acb2b55c
Hashan Sooriyage <hashansooriyage@gmail.com>
2025-06-04 11:53:20 +0530

    Initial commit from Spring Initializr

---
+++ src/main/java/com/loyalbridge/admin/LoyalbridgeAdminApplication.java
@@ -0,0 +1,13 @@
+package com.loyalbridge.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyalbridgeAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyalbridgeAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loyalbridge/admin/LoyalbridgeAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loyalbridge.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyalbridgeAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
